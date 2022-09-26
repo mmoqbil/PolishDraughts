907bcc1ac85f4dc358a646ede608e2220cbc52fc
Mateusz <mateusz.moqbil@gmail.com>
2022-09-26 14:28:21 +0200

    Board is displayed

---
+++ PolishDraughtsSolution/PolishDraughts/Board.cs
@@ -0,0 +1,13 @@
+﻿namespace PolishDraughts;
+
+public class Board
+{
+    public int Size { get; set; }
+
+    public string[,] CreateBoard(int BoardSize)
+    {
+        string[,] Board = new string[BoardSize, BoardSize];
+        return Board;
+    }
+}
+
--- PolishDraughtsSolution/PolishDraughts/Program.cs
+++ PolishDraughtsSolution/PolishDraughts/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using PolishDraughts;
+Console.WriteLine("Please put size of the board between 10-20: ");
+int BoardSize = int.Parse(Console.ReadLine());
+Console.WriteLine(BoardSize);
+PolishDraughts.Board board = new PolishDraughts.Board();
+board.Size = BoardSize;
+Console.WriteLine(board.Size);
+string[,] CreatedBoard = board.CreateBoard(board.Size);
+int count = 0;
+for (int row = 0; row<BoardSize; row++)
+{
+    for (int column = 0; column<BoardSize;column++)
+    {
+        if (row%2==0) 
+        {
+            if (column % 2 == 1)
+            {
+                CreatedBoard[row, column] = "B";
+            }
+            else
+            {
+                CreatedBoard[row, column] = "W";
+            }
+        }
+        else
+        {
+            if (column%2 == 0)
+            {
+                CreatedBoard[row, column] = "B";
+            }
+            else
+            {
+                CreatedBoard[row, column] = "W";
+            }
+        }
+        if (count == board.Size-1)
+        {
+            Console.WriteLine(CreatedBoard[row, column]);
+            count = 0;
+        }
+        else
+        {
+            Console.Write(CreatedBoard[row, column] + " ");
+            count++;
+        }
+
+    }
+}
+
