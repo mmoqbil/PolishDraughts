6427f4b5b2e95c3a304e678c6a46d81eae1f00b2
Mateusz <mateusz.moqbil@gmail.com>
2022-09-27 11:42:05 +0200

    New Pawns starting position

--- PolishDraughtsSolution/PolishDraughts/Board.cs
+++ PolishDraughtsSolution/PolishDraughts/Board.cs
@@ -34,9 +34,11 @@ public class Board
     }
     public void ToString(string position)
     {
+        
+        int x_pos = 0;
         foreach (var val in Enum.GetValues(typeof(pos_x)))
         {
-            Console.WriteLine(val);
+        
         }
     }
 }
