01f16b864a681e7a1c43a92225b1faa4598b2c11
Jakub Kuca <jakubkuca@outlook.com>
2022-09-27 14:24:08 +0200

    board with pawns but doesnt work

--- PolishDraughtsSolution/PolishDraughts/Board.cs
+++ PolishDraughtsSolution/PolishDraughts/Board.cs
@@ -4,7 +4,7 @@ public class Board
 {
     public int Size { get; set; }
 
-    public string[,] CreateBoard(int BoardSize)
+    public string[,] TwoDBoard(int BoardSize)
     {
         string[,] Board = new string[BoardSize, BoardSize];
         return Board;
@@ -16,7 +16,7 @@ public class Board
         return Board;
     }
 
-    public string[] DisplayBoard(string[] OneDBoard, int BoardSize)
+    public string[] MakeStringsBoard(string[] OneDBoard, int BoardSize, string[,] BoardWithPawns)
     {
 
         for (int row = 0; row < BoardSize; row++)
@@ -29,7 +29,7 @@ public class Board
                     {
                         OneDBoard[row] = string.Concat(Enumerable.Repeat("*** --- ", (BoardSize / 2)));
                         OneDBoard[row] += "\n";
-                        OneDBoard[row] += string.Concat(Enumerable.Repeat("* * - - ", (BoardSize / 2)));
+                        OneDBoard[row] += string.Concat(Enumerable.Repeat($"*{BoardWithPawns[row, column]}* -{BoardWithPawns[row, column]}- ", (BoardSize / 2)));
                         OneDBoard[row] += "\n";
                         OneDBoard[row] += string.Concat(Enumerable.Repeat("*** --- ", (BoardSize / 2)));
                     }
@@ -37,35 +37,36 @@ public class Board
                     {
                         OneDBoard[row] = string.Concat(Enumerable.Repeat("*** --- ", (int)(BoardSize / 2)));
                         OneDBoard[row] += "*** \n";
-                        OneDBoard[row] += string.Concat(Enumerable.Repeat("* * - - ", (int)(BoardSize / 2)));
-                        OneDBoard[row] += "* * \n";
+                        OneDBoard[row] += string.Concat(Enumerable.Repeat($"*{BoardWithPawns[row, column]}* -{BoardWithPawns[row, column]}- ", (int)(BoardSize / 2)));
+                        OneDBoard[row] += $"*{BoardWithPawns[row, column]}* \n";
                         OneDBoard[row] += string.Concat(Enumerable.Repeat("*** --- ", (int)(BoardSize / 2)));
                         OneDBoard[row] += "***";
                     }
-                    if (row % 2 == 1)
+                }
+
+                if (row % 2 == 1)
+                {
+                    if (BoardSize % 2 == 0)
                     {
-                        if (BoardSize % 2 == 0)
-                        {
-                            OneDBoard[row] = string.Concat(Enumerable.Repeat("--- *** ", (BoardSize / 2)));
-                            OneDBoard[row] += "\n";
-                            OneDBoard[row] += string.Concat(Enumerable.Repeat("- - * * ", (BoardSize / 2)));
-                            OneDBoard[row] += "\n";
-                            OneDBoard[row] += string.Concat(Enumerable.Repeat("--- *** ", (BoardSize / 2)));
-                        }
-                        else
-                        {
-                            OneDBoard[row] = string.Concat(Enumerable.Repeat("--- *** ", (int)(BoardSize / 2)));
-                            OneDBoard[row] += "--- \n";
-                            OneDBoard[row] += string.Concat(Enumerable.Repeat("- - * * ", (int)(BoardSize / 2)));
-                            OneDBoard[row] += "- - \n";
-                            OneDBoard[row] += string.Concat(Enumerable.Repeat("--- * * ", (int)(BoardSize / 2)));
-                            OneDBoard[row] += "---";
-                        }
+                        OneDBoard[row] = string.Concat(Enumerable.Repeat("--- *** ", (BoardSize / 2)));
+                        OneDBoard[row] += "\n";
+                        OneDBoard[row] += string.Concat(Enumerable.Repeat($"-{BoardWithPawns[row, column]}- *{BoardWithPawns[row, column]}* ", (BoardSize / 2)));
+                        OneDBoard[row] += "\n";
+                        OneDBoard[row] += string.Concat(Enumerable.Repeat("--- *** ", (BoardSize / 2)));
+                    }
+                    else
+                    {
+                        OneDBoard[row] = string.Concat(Enumerable.Repeat("--- *** ", (int)(BoardSize / 2)));
+                        OneDBoard[row] += "--- \n";
+                        OneDBoard[row] += string.Concat(Enumerable.Repeat($"-{BoardWithPawns[row, column]}- *{BoardWithPawns[row, column]}* ", (int)(BoardSize / 2)));
+                        OneDBoard[row] += $"-{BoardWithPawns[row, column]}- \n";
+                        OneDBoard[row] += string.Concat(Enumerable.Repeat("--- *** ", (int)(BoardSize / 2)));
+                        OneDBoard[row] += "---";
                     }
                 }
             }
-           
         }
+
         return OneDBoard;
     }
     public void ShowBoard(string[] DisplayBoard, int BoardSize)
@@ -76,6 +77,47 @@ public class Board
         }
         
     }
-}
 
-    
+    public string[,] PutPawnsOnBoard(string[,] EmptyBoard,int BoardSize)
+    {
+        for (int row = 0; row < BoardSize; row++)
+        {
+            for (int column = 0; column < BoardSize; column++)
+            {
+                if (row == BoardSize - 1 )
+                { 
+                    if (column % 2 == 1)
+                    {                     
+                        EmptyBoard[row, column] = "W";                              
+                    }
+                }
+                else if (row == BoardSize - 2)
+                {
+                    if (column % 2 == 0)
+                    {
+                        EmptyBoard[row, column] = "W";
+                    }
+                }
+                else if (row == 0)
+                {
+                    if (column % 2 == 0)
+                    {
+                        EmptyBoard[row, column] = "B";
+                    }
+                }
+                else if (row == 1)
+                {
+                    if (column % 2 == 1)
+                    {
+                        EmptyBoard[row, column] = "B";
+                    }
+                }
+                else
+                {
+                    EmptyBoard[row, column] = " ";
+                }
+            }
+        }
+        return EmptyBoard;
+    }
+}
--- PolishDraughtsSolution/PolishDraughts/Program.cs
+++ PolishDraughtsSolution/PolishDraughts/Program.cs
@@ -4,8 +4,9 @@ Console.WriteLine("Please put size of the board between 10-20: ");
 int BoardSize = int.Parse(Console.ReadLine());
 Board board = new Board();
 board.Size = BoardSize;
-string[,] CreatedBoard = board.CreateBoard(board.Size);
+string[,] BoardForPawns = board.TwoDBoard(board.Size);
 string[] OneDBoard = board.OneDBoard(board.Size);
-string[] DisplayBoard = board.DisplayBoard(OneDBoard, BoardSize);
-board.ShowBoard(DisplayBoard,BoardSize);
+string[,] BoardWithPawns = board.PutPawnsOnBoard(BoardForPawns, BoardSize);
+string[] MakeStringsBoard = board.MakeStringsBoard(OneDBoard, BoardSize, BoardWithPawns);
+board.ShowBoard(MakeStringsBoard, BoardSize);
 
